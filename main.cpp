006255aacea4657a46d7b283693f63e44c91dd98
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-11-26 18:14:59 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int massimo (int n1, int n2)
+{
+    int max;
+    if (n1 >n2)
+    {
+        max =n1;
+    }
+    else
+    {
+        max = n2;
+    }
+    return max;
 }
-
+void main()
+{
+    int n1, n2, n3, n4, max;
+    cout << "inserisci 4 valori" << endl;
+    cin >>n1>>n2>>n3>>n4;
+    max =massimo(n1,n2);
+    max = massimo ( n3, max);
+    max = massimo (n4, max);
+    cout << "il valore massimo è " << max  << endl;
+}
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
